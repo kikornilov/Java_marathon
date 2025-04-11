d942440fb3e3ddb0c811cd000fc12e4377727d43
kikornilov <52114814+kikornilov@users.noreply.github.com>
2025-04-11 16:16:17 +0300

    Добавление в первую программу из книги второй программы

--- src/FirstLesson.java
+++ src/FirstLesson.java
@@ -7,5 +7,9 @@ public class FirstLesson {
     // Программа начинается с вызова main()
     public static void main(String[] args) {
         System.out.println("Простая программа на языке Java!");
+        int num = 100; // Объявление переменной num
+        System.out.println("Значение num: " + num);
+        num = num * 2;
+        System.out.println("Значение num * 2 = " + num);
     }
 }
